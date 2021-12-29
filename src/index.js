f538d7a1403e86c8f0dd9e385e3d7098d32df8c9
Mangaliso <mangaliso.twala@gmail.com>
2021-12-29 12:25:15 +0200

    add number template literal Add number template literal and container div

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const name = "Mangaliso";
+const number = 7;
+
+ReactDOM.render(
+  <div>
+    <h1>Hello {name}!</h1>
+    <p>Lucky number {number}</p>
+  </div>,
+  document.getElementById("root")
+);
